002c2957baefe72f4407fb32a1bfda3276769d4c
henrytran1604 <quanghuy1604nd@gmail.com>
2024-07-04 23:11:32 +0700

    update

--- src/components/log/LogArea.jsx
+++ src/components/log/LogArea.jsx
@@ -7,13 +7,26 @@ const LogArea = (props) => {
 
     const [messages, setMessages] = useState([]);
 
+
     useEffect(() => {
+        const fetchHistoryLog = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/log-message');
+                const data = await response.json();
+                setMessages(data.reverse());
+            } catch (err) {
+                
+            }
+        };
+
+        fetchHistoryLog();
+
         const socket = new SockJS('http://localhost:8080/ws');
         const client = Stomp.over(socket);
 
         client.connect({}, () => {
             client.subscribe('/topic/log', (msg) => {
-                setMessages((prevMessages) => [...prevMessages, msg.body]);
+                setMessages((prevMessages) => [msg.body, ...prevMessages]);
             });
         });
 
@@ -27,7 +40,7 @@ const LogArea = (props) => {
     return (
         <div className='mt-4 border-t-2 border-[#DDDDE3] border-solid h-[90%] p-4 overflow-y-scroll'>
             {messages.filter((value, _) => value.includes(filterValue)).map((message, index) => (
-                <div key={index}>{message}</div>
+                <div key={index} className='text-ellipsis overflow-hidden whitespace-nowrap'>{message}</div>
             ))}
         </div>
     );
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,11 +1,23 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { getIP } from '../services/UserServices';
+import { toast } from 'react-toastify';
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState({});
     const [isLoading, setIsLoading] = useState(false);
+    const [IP, setIP] = useState('Detecting...')
+    
+    useEffect(() => {
+        const fetchIP = async () => {
+            const userIP = await getIP();
+            setIP(userIP);
+        }
+        fetchIP();
+    }, [])
+    
     const navigate = useNavigate()
     // const register = () => {
         
@@ -14,14 +26,24 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const foundUser = localStorage.getItem('ltm');
         if(foundUser) {
-            setUser(JSON.parse(foundUser))
+            const jsonUser = JSON.parse(foundUser)
+            if(jsonUser.ip !== IP) {
+                toast.error('Mã sinh viên và IP không trùng khớp!', {
+                    autoClose: 2000
+                })
+                localStorage.removeItem('ltm')
+                navigate('/register')
+            }
+            else {
+                setUser(jsonUser)
+            }
             setIsLoading(false)
         } else {
             setIsLoading(false)
             navigate('/register')
         }
 
-    }, [navigate])
+    }, [navigate, IP])
 
     return (
         <AuthContext.Provider value={{ user }}>
